04ba9e46b36d84236815b8539151b83748602a03
leeseoeun <seoeun.shuen@gmail.com>
2024-12-14 10:10:05 +0900

    Initial commit: Welcome to JEJU v2

---
+++ src/main/java/com/welcometojejuv2/WelcomeToJejuV2Application.java
@@ -0,0 +1,13 @@
+package com.welcometojejuv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeToJejuV2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WelcomeToJejuV2Application.class, args);
+  }
+
+}
