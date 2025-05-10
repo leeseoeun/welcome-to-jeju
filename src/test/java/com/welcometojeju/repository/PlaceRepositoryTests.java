0d536d78444d7913710d251bb98b2bfa9e48bd22
leeseoeun <seoeun.shuen@gmail.com>
2025-05-10 21:55:09 +0900

    test: 장소 등록 기능 테스트 코드 작성

---
+++ src/test/java/com/welcometojeju/repository/PlaceRepositoryTests.java
@@ -0,0 +1,61 @@
+package com.welcometojeju.repository;
+
+import com.welcometojeju.domain.Place;
+import com.welcometojeju.domain.Theme;
+import com.welcometojeju.domain.ThemePlace;
+import com.welcometojeju.domain.User;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class PlaceRepositoryTests {
+
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private ThemeRepository themeRepository;
+  @Autowired
+  private PlaceRepository placeRepository;
+  @Autowired
+  private ThemePlaceRepository themePlaceRepository;
+
+  @Test
+  public void testCreateTheme() {
+    Optional<User> userResult = userRepository.findById(1);
+    User user = userResult.orElseThrow();
+
+    IntStream.rangeClosed(1, 1000).forEach(i -> {
+      Place place = Place.builder()
+          .no(i)
+          .name("name" + i)
+          .address("address" + i)
+          .phone("phone" + i)
+          .x(String.valueOf(126.51586040620134 + 0.1))
+          .y(String.valueOf(33.23751957595788 + 0.1))
+          .registerCount(0)
+          .user(user)
+          .build();
+
+      Place placeResult = placeRepository.save(place);
+
+      placeResult.incrementRegisterCount();
+
+      Optional<Theme> themeResult = themeRepository.findById(1);
+      Theme theme = themeResult.orElseThrow();
+
+      ThemePlace themePlace = ThemePlace.builder()
+          .theme(theme)
+          .place(placeResult)
+          .build();
+
+      themePlaceRepository.save(themePlace);
+    });
+  }
+
+}
