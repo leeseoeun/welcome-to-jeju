0bf1e23b8a9c0069f9fccf6e3cf2852eef6c8459
leeseoeun <seoeun.shuen@gmail.com>
2025-05-13 13:18:04 +0900

    perf: 메인 페이지에서 데이터 조회 시 비동기 병렬 처리로 성능 개선 (CompletableFuture)

--- src/main/java/com/welcometojeju/controller/HomeController.java
+++ src/main/java/com/welcometojeju/controller/HomeController.java
@@ -12,8 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,24 +26,67 @@ public class HomeController {
   private final UserService userService;
   private final ThemeService themeService;
   private final PlaceService placeService;
+  private final View error;
 
   @GetMapping
   public String home(Model model) {
-    List<UserDTO> users = userService.getTop3UsersByViewCount();
-    model.addAttribute("users", users);
-    log.info("[home > users] " + users);
+    long beforeTime = System.currentTimeMillis();
 
-    List<ThemeDTO> publicThemes = themeService.getTop3PublicThemesByViewCount();
-    model.addAttribute("publicThemes", publicThemes);
-    log.info("[home > publicThemes] " + publicThemes);
+    // 병렬로 여러 작업 수행 후 결과를 모아야 하기 때문에 CompletableFuture 사용
+    CompletableFuture<List<UserDTO>> users =
+        CompletableFuture.supplyAsync(() -> userService.getTop3UsersByViewCount())
+            .whenComplete((result, error) -> {
+              if (error == null) {
+                model.addAttribute("users", result);
+                log.info("[home > users] {}", result);
+              } else {
+                log.error("[home > users] ", error);
+              }
+            });
 
-    List<ThemeDTO> collaborateThemes = themeService.getTop3CollaborateThemesByViewCount();
-    model.addAttribute("collaborateThemes", collaborateThemes);
-    log.info("[home > collaborateThemes] " + collaborateThemes);
+    CompletableFuture<List<ThemeDTO>> publicThemes =
+        CompletableFuture.supplyAsync(() -> themeService.getTop3PublicThemesByViewCount())
+            .whenComplete((result, error) -> {
+              if (error == null) {
+                model.addAttribute("publicThemes", result);
+                log.info("[home > publicThemes] {}", result);
+              } else {
+                log.error("[home > publicThemes] ", error);
+              }
+            });
 
-    List<PlaceDTO> places = placeService.getTop3PlacesByRegisterCount();
-    model.addAttribute("places", places);
-    log.info("[home > places] " + places);
+    CompletableFuture<List<ThemeDTO>> collaborateThemes =
+        CompletableFuture.supplyAsync(() -> themeService.getTop3CollaborateThemesByViewCount())
+            .whenComplete((result, error) -> {
+              if (error == null) {
+                model.addAttribute("collaborateThemes", result);
+                log.info("[home > collaborateThemes] {}", result);
+              } else {
+                log.error("[home > collaborateThemes] ", error);
+              }
+            });
+
+    CompletableFuture<List<PlaceDTO>> places =
+        CompletableFuture.supplyAsync(() -> placeService.getTop3PlacesByRegisterCount())
+            .whenComplete((result, error) -> {
+              if (error == null) {
+                model.addAttribute("places", result);
+                log.info("[home > places] {}", result);
+              } else {
+                log.error("[home > places] ", error);
+              }
+            });
+
+    CompletableFuture.allOf(
+        users,
+        publicThemes,
+        collaborateThemes,
+        places
+    ).join();
+
+    long afterTime = System.currentTimeMillis();
+    long diffTime = afterTime - beforeTime;
+    log.info("[home > diffTime] 실행 시간 : " + diffTime + "ms");
 
     return "home";
   }
